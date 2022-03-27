3b0509f822e080ab5f411ac24588e227b0fa08a1
stolomeo <tolomeosebastian@gmail.com>
2022-03-27 13:48:59 -0400

    Add tabs to index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
-import pageLoad from "./pageload";
-pageLoad();
+import homeLoad from "./home";
+
+const createHeader = () => {
+  const header = document.createElement("header");
+
+  const titleDiv = document.createElement("div");
+  titleDiv.classList.add("title");
+  titleDiv.textContent = `Boomin' Breakfast`;
+  header.appendChild(titleDiv);
+
+  const navEl = document.createElement("nav");
+
+  const homeBtn = document.createElement("button");
+  homeBtn.textContent = "Home";
+  navEl.appendChild(homeBtn);
+
+  const menuBtn = document.createElement("button");
+  menuBtn.textContent = "Menu";
+  navEl.appendChild(menuBtn);
+
+  const aboutBtn = document.createElement("button");
+  aboutBtn.textContent = "About";
+  navEl.appendChild(aboutBtn);
+
+  header.appendChild(navEl);
+
+  const content = document.querySelector("#content");
+  content.appendChild(header);
+
+  return header;
+};
+createHeader();
+homeLoad();
