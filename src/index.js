aa58c13fe494d3084c6f417946c9a362aaf8f6fb
stolomeo <tolomeosebastian@gmail.com>
2022-03-29 13:43:33 -0400

    Added <main> as parent for each file's content. Added tab functionality

--- src/index.js
+++ src/index.js
@@ -3,35 +3,74 @@ import menuLoad from "./menu";
 import aboutLoad from "./about";
 
 const createHeader = () => {
-  const header = document.createElement("header");
+  const headerEl = document.createElement("header");
 
   const titleDiv = document.createElement("div");
   titleDiv.classList.add("title");
-  titleDiv.textContent = `Boomin' Breakfast`;
-  header.appendChild(titleDiv);
+  titleDiv.textContent = "Boomin` Breakfast";
+  headerEl.appendChild(titleDiv);
 
+  const nav = createNav();
+  headerEl.appendChild(nav);
+  return headerEl;
+};
+
+const createNav = () => {
   const navEl = document.createElement("nav");
+  navEl.classList.add("nav");
 
   const homeBtn = document.createElement("button");
   homeBtn.textContent = "Home";
-  navEl.appendChild(homeBtn);
+  homeBtn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(homeBtn);
+    homeLoad();
+  });
 
   const menuBtn = document.createElement("button");
   menuBtn.textContent = "Menu";
-  navEl.appendChild(menuBtn);
-
+  menuBtn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(menuBtn);
+    menuLoad();
+  });
   const aboutBtn = document.createElement("button");
   aboutBtn.textContent = "About";
-  navEl.appendChild(aboutBtn);
+  aboutBtn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(aboutBtn);
+    aboutLoad();
+  });
 
-  header.appendChild(navEl);
-
-  const content = document.querySelector("#content");
-  content.appendChild(header);
-
-  return header;
+  navEl.append(homeBtn, menuBtn, aboutBtn);
+  return navEl;
 };
-createHeader();
+
+const content = document.querySelector("#content");
+content.append(createHeader());
+
+const createMain = () => {
+  const main = document.createElement("main");
+  return main;
+};
+
+content.append(createMain());
+const setActiveButton = () => {
+  let buttons = document.querySelectorAll(".nav button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      resetBtns();
+      button.classList.add("active");
+    });
+  });
+  return buttons;
+};
+
+const resetBtns = () => {
+  setActiveButton().forEach((button) => {
+    button.classList.remove("active");
+  });
+};
+
+console.log(resetBtns());
 homeLoad();
-menuLoad();
-aboutLoad();
